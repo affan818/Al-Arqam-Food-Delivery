4681c45771d131c71a046d29cf8a4500ff359b9a
affan818 <ansariaffan818@gmail.com>
2024-02-19 14:35:18 +0530

    change into faded and interval time

---
+++ Al-Arqam/src/components/Carousel.jsx
@@ -0,0 +1,81 @@
+
+const Carousel = () => {
+  return (
+    <div>
+      <div
+        id="carouselExampleInterval"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner" id="carousel">
+          <div className="carousel-caption" style={{ zIndex: "10" }}>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                style={{ backgroundColor: "ThreeDFace" }}
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="carousel-item active" data-bs-interval={2000}>
+            <img
+              src="https://source.unsplash.com/random/300×300/?kabab"
+              className="d-block w-100"
+              alt="..."
+              style={{ filter: "brightness(30%)", height: "600px" }}
+            />
+          </div>
+          <div className="carousel-item" data-bs-interval={2000}>
+            <img
+              src="https://source.unsplash.com/random/300×300/?dumbiryani"
+              className="d-block w-100"
+              alt="..."
+              style={{ filter: "brightness(30%)", height: "600px" }}
+            />
+          </div>
+          <div className="carousel-item" data-bs-interval={2000}>
+            <img
+              src="https://source.unsplash.com/random/300×500/?frychicken"
+              className="d-block w-100"
+              alt="..."
+              style={{ filter: "brightness(30%)", height: "600px" }}
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://source.unsplash.com/random/300×500/?mutton"
+              className="d-block w-100"
+              alt="..."
+              style={{ filter: "brightness(30%)", height: "600px" }}
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
