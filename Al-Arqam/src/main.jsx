e3970245a5d4b1ebdb60e431c509eda3ad033afd
affan818 <ansariaffan818@gmail.com>
2024-02-19 14:34:36 +0530

    update router

--- Al-Arqam/src/main.jsx
+++ Al-Arqam/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <App />
-  </Router>,
-)
+  </Router>
+);
