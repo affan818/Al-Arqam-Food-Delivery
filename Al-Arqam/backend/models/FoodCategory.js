24c9a41fd0c7596b780491478f2a555b09dbd7f3
affan818 <ansariaffan818@gmail.com>
2024-02-25 01:44:17 +0530

    Delet Backend Because some big Errors

--- Al-Arqam/backend/models/FoodCategory.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const foodcateSchema = new mongoose.Schema({
-  CategoryName: { type: String, require: true, trim: true },
-});
-
-const foodCategoryModel = mongoose.model("foodCategory", foodcateSchema);
-
-export default foodCategoryModel;
