f238c598c6e5b8cbf08356c7f77e713294ed065a
affan818 <ansariaffan818@gmail.com>
2024-02-29 21:56:18 +0530

    new login components

--- Al-Arqam/src/App.jsx
+++ Al-Arqam/src/App.jsx
@@ -5,16 +5,18 @@ import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div style={{backgroundColor:'white',color:'black'}}>
+    <div style={{ backgroundColor: "white", color: "black" }}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </div>
--- Al-Arqam/src/pages/Login.jsx
+++ Al-Arqam/src/pages/Login.jsx
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
 import "./signup.css";
-import { FaUserAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { IoMail } from "react-icons/io5";
 import { RiLockPasswordLine } from "react-icons/ri";
-import { Link } from "react-router-dom";
 
-const Signup = () => {
+const Login = () => {
   return (
     <>
       <div className="container signup">
         <div className="header">
-          <div className="text text-success">Sign Up</div>
+          <div className="text text-success">Login</div>
           <div className="underline"></div>
         </div>
         <div className="inputs">
-          <div className="input">
-            <label id="name">
-              <FaUserAlt />
-            </label>
-            <input
-              className="text-dark"
-              id="name"
-              placeholder="Name"
-              type="text"
-            />
-          </div>
-
           <div className="input">
             <label id="email">
               <IoMail />
@@ -49,23 +35,20 @@ const Signup = () => {
             />
           </div>
         </div>
-
-        {/* <div className="forgot-password">
-            forgot password ? <span>cicck here!</span>
-          </div> */}
-
+        <div className="forgot-password">
+          forgot password ? <span>cicck here!</span>
+        </div>
         <div className="submit-container">
-          <div className="submit">Sign Up</div>
-
           <div className="submit gray">
-            <Link className="nav-link" to={"/login"}>
-              Login
+            <Link className="nav-link" to={"/signup"}>
+              Sign up
             </Link>
           </div>
+          <div className="submit">Login</div>
         </div>
       </div>
     </>
   );
 };
 
-export default Signup;
+export default Login;
