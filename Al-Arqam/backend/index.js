579e25cd943b9bc31c42315e5147c8ddb678de53
affan818 <ansariaffan818@gmail.com>
2024-03-02 07:21:50 +0530

    install cors and some minar changes in router

--- Al-Arqam/backend/index.js
+++ Al-Arqam/backend/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
 import connectDB from "./DB/connection.js";
 import createUserRoute from "./routes/CreateUserRoute.js";
 // import createUser from "./CRUD/create.js";
-dotenv.config();
 const app = express();
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
@@ -11,8 +12,9 @@ const port = process.env.PORT || 5000;
 const localURL = process.env.localURL || "localhost";
 
 connectDB();
+app.use(cors());
 app.use(express.json());
-app.use("/user", createUserRoute);
+app.use(createUserRoute);
 // createUser()
 app.get("/", (req, res) => {
   res.send("<h1>Wellcome</h1>");
--- Al-Arqam/backend/routes/CreateUserRoute.js
+++ Al-Arqam/backend/routes/CreateUserRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { body, validationResult } from "express-validator";
-import createUser from "../CRUD/create.js";
+import userModel from "../models/UserModel.js";
+// import createUser from "../CRUD/create.js";
 const router = express.Router();
 
 //get props and destructure
 router.post(
-  "/create",
+  "/register",
   body("email", "invalid email").isEmail(),
   // password must be at least 5 chars long
   body("password", "password must have five character").isLength({ min: 5 }),
@@ -14,15 +15,25 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     } else {
-      await createUser(
-        req.body.name,
-        req.body.email,
-        req.body.password,
-        req.body.location
-      );
+      await userModel.create(req.body);
       res.send({ success: true });
     }
   }
 );
+router.post("/login", (req, res) => {
+  const {email, password} = req.body;
+  userModel.findOne({email : email})
+  .then(user => {
+      if(user) {
+          if(user.password === password){
+              res.json("Success")
+          }else{
+              res.json("The password is incorrect")
+          }
+      }else{
+          res.json("No record existed")
+      }
+  })
+})
 
 export default router;
