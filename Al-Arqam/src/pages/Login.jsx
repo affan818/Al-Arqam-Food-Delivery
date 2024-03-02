f504040c3a41cc9f8935573b17b2454bf41c92ea
affan818 <ansariaffan818@gmail.com>
2024-03-02 07:53:25 +0530

    connect form to backend

--- Al-Arqam/src/pages/Login.jsx
+++ Al-Arqam/src/pages/Login.jsx
@@ -2,8 +2,31 @@ import "./signup.css";
 import { Link } from "react-router-dom";
 import { IoMail } from "react-icons/io5";
 import { RiLockPasswordLine } from "react-icons/ri";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/login", { email, password })
+      .then((result) => {
+        console.log(result);
+        if (result.data === "Success") {
+          navigate("/");
+        } else {
+          navigate("/signup");
+          alert(result.data);
+        }
+      })
+      .catch((err) => {
+        console.log(`ERROR:-${err}`);
+      });
+  };
   return (
     <>
       <div className="container signup">
@@ -11,41 +34,47 @@ const Login = () => {
           <div className="text text-success">Login</div>
           <div className="underline"></div>
         </div>
-        <div className="inputs">
-          <div className="input">
-            <label id="email">
-              <IoMail />
-            </label>
-            <input
-              className="text-dark"
-              id="email"
-              type="email"
-              placeholder="Email Id"
-            />
+        <form action="" onSubmit={handleSubmit}>
+          <div className="inputs">
+            <div className="input">
+              <label id="email">
+                <IoMail />
+              </label>
+              <input
+                className="text-dark"
+                id="email"
+                type="email"
+                placeholder="Email Id"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="input">
+              <label id="password">
+                <RiLockPasswordLine />
+              </label>
+              <input
+                className="text-dark"
+                id="password"
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
           </div>
-          <div className="input">
-            <label id="password">
-              <RiLockPasswordLine />
-            </label>
-            <input
-              className="text-dark"
-              id="password"
-              type="password"
-              placeholder="Password"
-            />
+          <div className="forgot-password">
+            forgot password ? <span>cicck here!</span>
           </div>
-        </div>
-        <div className="forgot-password">
-          forgot password ? <span>cicck here!</span>
-        </div>
-        <div className="submit-container">
-          <div className="submit gray">
-            <Link className="nav-link" to={"/signup"}>
-              Sign up
-            </Link>
+          <div className="submit-container">
+            <div className="submit gray">
+              <Link className="nav-link" to={"/signup"}>
+                Sign up
+              </Link>
+            </div>
+            <button type="submit" className="submit">
+              Login
+            </button>
           </div>
-          <div className="submit">Login</div>
-        </div>
+        </form>
       </div>
     </>
   );
--- Al-Arqam/src/pages/Signup.jsx
+++ Al-Arqam/src/pages/Signup.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
 import "./signup.css";
 import { FaUserAlt } from "react-icons/fa";
 import { IoMail } from "react-icons/io5";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/register", { name, email, password })
+      .then((result) => {
+        console.log(result);
+        navigate("/login");
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <>
       <div className="container signup">
@@ -13,56 +30,65 @@ const Signup = () => {
           <div className="text text-success">Sign Up</div>
           <div className="underline"></div>
         </div>
-        <div className="inputs">
-          <div className="input">
-            <label id="name">
-              <FaUserAlt />
-            </label>
-            <input
-              className="text-dark"
-              id="name"
-              placeholder="Name"
-              type="text"
-            />
-          </div>
+        <form action="" onSubmit={handleSubmit}>
+          <div className="inputs">
+            <div className="input">
+              <label id="name">
+                <FaUserAlt />
+              </label>
+              <input
+                className="text-dark"
+                id="name"
+                name="name"
+                placeholder="Username"
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
 
-          <div className="input">
-            <label id="email">
-              <IoMail />
-            </label>
-            <input
-              className="text-dark"
-              id="email"
-              type="email"
-              placeholder="Email Id"
-            />
+            <div className="input">
+              <label id="email">
+                <IoMail />
+              </label>
+              <input
+                className="text-dark"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Email Id"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="input">
+              <label id="password">
+                <RiLockPasswordLine />
+              </label>
+              <input
+                className="text-dark"
+                id="password"
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
           </div>
-          <div className="input">
-            <label id="password">
-              <RiLockPasswordLine />
-            </label>
-            <input
-              className="text-dark"
-              id="password"
-              type="password"
-              placeholder="Password"
-            />
+          <div className="forgot-password">
+          Already have an account? 
+            <span>
+            Login
+            </span>
           </div>
-        </div>
-
-        {/* <div className="forgot-password">
-            forgot password ? <span>cicck here!</span>
-          </div> */}
-
-        <div className="submit-container">
-          <div className="submit">Sign Up</div>
-
-          <div className="submit gray">
-            <Link className="nav-link" to={"/login"}>
-              Login
-            </Link>
+          <div className="submit-container">
+            <button type="submit" className="submit">
+              Sign Up
+            </button>
+            <div className="submit gray">
+              <Link className="nav-link" to={"/login"}>
+                Login
+              </Link>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
