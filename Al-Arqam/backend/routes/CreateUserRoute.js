f23c889451bf9cc6055f318cbd533aff4b288267
affan818 <ansariaffan818@gmail.com>
2024-03-02 07:45:55 +0530

    Edit error message

--- Al-Arqam/backend/routes/CreateUserRoute.js
+++ Al-Arqam/backend/routes/CreateUserRoute.js
@@ -21,19 +21,18 @@ router.post(
   }
 );
 router.post("/login", (req, res) => {
-  const {email, password} = req.body;
-  userModel.findOne({email : email})
-  .then(user => {
-      if(user) {
-          if(user.password === password){
-              res.json("Success")
-          }else{
-              res.json("The password is incorrect")
-          }
-      }else{
-          res.json("No record existed")
+  const { email, password } = req.body;
+  userModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("The password is incorrect");
       }
-  })
-})
+    } else {
+      res.json("No record existed or enter a valid email");
+    }
+  });
+});
 
 export default router;
