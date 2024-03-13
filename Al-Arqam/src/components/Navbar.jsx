3db3fa4b32fc6e1c4f910c38b824ab9a3f3000a4
affan818 <ansariaffan818@gmail.com>
2024-03-13 06:59:34 +0530

    Add Nav logic and some style

--- Al-Arqam/src/components/Navbar.jsx
+++ Al-Arqam/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import icon from "../assets/favIcon.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  };
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-success">
@@ -24,52 +29,58 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
               <li className="nav-item ">
-                <Link className="nav-link" style={{ color: "black" }} to={"/"}>
+                <Link className="nav-link active fs-5 text-white" to={"/"}>
                   Home
                 </Link>
               </li>
+              {localStorage.getItem("authToken") ? (
+                <li className="nav-item ">
+                  <Link className="nav-link  text-white" to={"/myorder"}>
+                    My Order
+                  </Link>
+                </li>
+              ) : (
+                ""
+              )}
               <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  style={{ color: "black" }}
-                  to={"/about"}
-                >
+                <Link className="nav-link text-white " to={"/about"}>
                   About
                 </Link>
               </li>
               <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  style={{ color: "black" }}
-                  to={"/contact"}
-                >
+                <Link className="nav-link text-white " to={"/contact"}>
                   Contact
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  style={{ color: "black" }}
-                  to={"/signup"}
-                >
-                  Signup
-                </Link>
-              </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2 bg-transparent text-dark"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button
-                className="btn btn-outline-warning text-dark"
-                type="submit"
-              >
-                Search
-              </button>
-            </form>
+            <div className="d-flex">
+              {!localStorage.getItem("authToken") ? (
+                <div>
+                  <Link
+                    className="btn bg-white text-success mx-1"
+                    to={"/login"}
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    className="btn bg-white text-success mx-1"
+                    to={"/signup"}
+                  >
+                    SignUp
+                  </Link>
+                </div>
+              ) : (
+                <>
+                  <div className="btn bg-white text-success mx-2">My Cart</div>
+                  <div
+                    className="btn bg-white text-danger mx-2"
+                    onClick={handleLogOut}
+                  >
+                    Logout
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </nav>
